f5cc67e65901251b94a9972ba27fc766db443d4c
marinodilauro <m.dilauro90@gmail.com>
2024-07-25 11:13:06 +0200

    Add props destructuring

--- src/index.js
+++ src/index.js
@@ -85,17 +85,17 @@ function Menu() {
 }
 
 // Pizza component
-function Pizza(props) {
+function Pizza({ pizzaObj }) {
 
-  if (props.pizzaObj.soldOut) return null;
+  if (pizzaObj.soldOut) return null;
 
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price}</span>
       </div>
     </li>
   );
@@ -128,11 +128,11 @@ function Footer() {
 }
 
 // Order CTA component
-function Order(props) {
+function Order(closeHour) {
   return (
     <div className="order">
       <p>
-        We're open until {props.closeHour}:00. Come visit us or order online.
+        We're open until {closeHour}:00. Come visit us or order online.
       </p>
       <button className="btn">Order</button>
     </div>
